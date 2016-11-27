a6af077b8707835b4e3be663b097de4646305cea
Waleed97 <Waleed97@users.noreply.github.com>
2016-11-27 13:56:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ angular.module('NarrowItDownApp',[])
 .service('MenuSearchService', MenuSearchService)
 .directive('foundItems',FoundItems);//should disply found list
 function FoundItems(){
+   
    var ddo={
     templateUrl:'foundItems.html',
     scope:{
